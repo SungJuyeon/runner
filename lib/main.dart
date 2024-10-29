53019108622fddd536d6e5d79abc5a7513a97198
SoungJuyeon <144316432+SungJuyeon@users.noreply.github.com>
2024-10-29 22:45:43 +0900

    폰트 지정

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'pages/temp_startPage.dart';
 import 'pages/ranking_page.dart';
+import 'pages/home.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,12 +13,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Multi-Page App',
       theme: ThemeData(
+        fontFamily: 'dohyeon', // 전체 폰트 지정
         primarySwatch: Colors.blue,
       ),
+      themeMode: ThemeMode.system,
       initialRoute: '/',
       routes: {
         '/': (context) => temp_startPage(),
         '/ranking': (context) => ranking_page(),
+        '/home': (context) => HomePage(),
       },
     );
   }
