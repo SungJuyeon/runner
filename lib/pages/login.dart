4a8b6698d2a4ffa92de76b241d1b3a491f485314
SoungJuyeon <144316432+SungJuyeon@users.noreply.github.com>
2024-11-04 12:51:59 +0900

    디자인 수정

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,26 +1,17 @@
 import 'package:flutter/material.dart';
 
+import 'navigationBar.dart';
+
 class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFF67A4FB), // 배경색 설정
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 1, // '홈' 탭 선택
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.campaign),
-            label: '랭킹',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: '홈',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: '마이',
-          ),
-        ],
+      bottomNavigationBar: buildBottomNavigationBar(
+        context,
+        onHomePressed,
+        onRankingPressed,
+        onProfilePressed,
       ),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
@@ -37,7 +28,7 @@ class LoginScreen extends StatelessWidget {
               width: 150, // 이미지 너비 설정
               height: 100, // 이미지 높이 설정
             ),
-            SizedBox(height: 20),
+            SizedBox(height: 40),
             // 첫 번째 Row: 아이디
             Row(
               children: [
@@ -47,13 +38,25 @@ class LoginScreen extends StatelessWidget {
                     ' 아이디',
                     style: TextStyle(
                       color: Color(0xFFF0EC7D),
-                      fontWeight: FontWeight.bold,
-                      fontSize: 25,
+                      //fontWeight: FontWeight.bold,
+                      fontSize: 22,
                     ),
                   ),
                 ),
                 SizedBox(width: 20), // 레이블과 TextField 사이 간격
                 Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black54.withOpacity(0.5), // Shadow color with transparency
+                          spreadRadius: 0, // How much the shadow spreads
+                          blurRadius: 5, // The blur intensity of the shadow
+                          offset: Offset(0, 3), // Shadow position
+                        ),
+                      ],
+                      borderRadius: BorderRadius.circular(30.0), // Match the TextField's border radius
+                    ),
                   child: TextField(
                     decoration: InputDecoration(
                       filled: true,
@@ -63,6 +66,7 @@ class LoginScreen extends StatelessWidget {
                         borderSide: BorderSide.none, // 테두리 제거
                       ),
                     ),
+                  ),
                   ),
                 ),
               ],
@@ -77,28 +81,41 @@ class LoginScreen extends StatelessWidget {
                     '비밀번호',
                     style: TextStyle(
                       color: Color(0xFFF0EC7D),
-                      fontWeight: FontWeight.bold,
-                      fontSize: 25,
+                      //fontWeight: FontWeight.bold,
+                      fontSize: 22,
                     ),
                   ),
                 ),
                 SizedBox(width: 20), // 레이블과 TextField 사이 간격
                 Expanded(
-                  child: TextField(
-                    obscureText: true, // 비밀번호 입력 가리기
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.white, // 필드 배경 흰색
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30.0),
-                        borderSide: BorderSide.none, // 테두리 제거
+                  child: Container(
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black54.withOpacity(0.5), // Shadow color with transparency
+                          spreadRadius: 0, // How much the shadow spreads
+                          blurRadius: 5, // The blur intensity of the shadow
+                          offset: Offset(0, 3), // Shadow position
+                        ),
+                      ],
+                      borderRadius: BorderRadius.circular(30.0), // Match the TextField's border radius
+                    ),
+                    child: TextField(
+                      obscureText: true, // Hide password input
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white, // Field background color
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30.0),
+                          borderSide: BorderSide.none, // Remove border
+                        ),
                       ),
                     ),
                   ),
                 ),
               ],
             ),
-            SizedBox(height: 20),
+            SizedBox(height: 40),
             ElevatedButton(
               onPressed: () {
                 // 로그인 로직 처리
@@ -110,10 +127,16 @@ class LoginScreen extends StatelessWidget {
                   side: BorderSide.none,  // 테두리 제거
                 ),
                 padding: EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+                shadowColor: Colors.grey.withOpacity(0.5), // Shadow color with transparency
+                elevation: 10,
               ),
               child: Text(
                 '    로그인    ',
-                style: TextStyle(color: Color(0xFF306DFB), fontSize: 22),
+                style: TextStyle(
+                    color: Color(0xFF3A3934),
+                    fontSize: 20,
+                    //fontWeight: FontWeight.bold
+                ),
               ),
             ),
             SizedBox(height: 20),
@@ -125,13 +148,19 @@ class LoginScreen extends StatelessWidget {
               style: ElevatedButton.styleFrom(
                 backgroundColor: Color(0xFFF0EC7D), // 배경 색
                 shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30.0), // 버튼 둥글게
+                  borderRadius: BorderRadius.circular(25.0), // 버튼 둥글게
                   side: BorderSide.none, // 테두리 제거
                 ),
-                padding: EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                shadowColor: Colors.grey.withOpacity(0.5), // Shadow color with transparency
+                elevation: 10,
               ),
               child: Text('   회원가입   ',
-                style: TextStyle(color: Colors.black, fontSize: 22),
+                style: TextStyle(
+                    color: Color(0xFF3A3934),
+                    fontSize: 20,
+                    //fontWeight: FontWeight.bold
+                ),
               ),
             ),
           ],
--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -1,26 +1,17 @@
 import 'package:flutter/material.dart';
 
+import 'navigationBar.dart';
+
 class SignUpScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFF66A2FD), // 배경색 설정
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 1, // '홈' 탭 선택
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.campaign),
-            label: '랭킹',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: '홈',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: '마이',
-          ),
-        ],
+      bottomNavigationBar: buildBottomNavigationBar(
+        context,
+        onHomePressed,
+        onRankingPressed,
+        onProfilePressed,
       ),
       body: Center(
         child: Padding(
@@ -92,27 +83,28 @@ class SignUpScreen extends StatelessWidget {
     return Row(
       children: [
         Container(
-          width: 100, // 레이블 크기를 고정
+          width: 80, // 레이블 크기를 고정
           child: Text(
             label,
             style: TextStyle(
               color: Colors.yellow,
               fontWeight: FontWeight.bold,
+              fontSize: 20,
             ),
           ),
         ),
         SizedBox(width: 20), // 레이블과 TextField 사이 간격
         Expanded(
           child: Material(
-            elevation: 5, // 그림자 크기
+            elevation: 8, // 그림자 크기
             borderRadius: BorderRadius.circular(30.0),
-            shadowColor: Colors.black26, // 그림자 색상
+            shadowColor: Colors.black87, // 그림자 색상
             child: TextField(
               obscureText: obscureText, // 비밀번호 입력 가리기
               decoration: InputDecoration(
                 filled: true,
                 fillColor: Colors.white, // 필드 배경 흰색
-                contentPadding: EdgeInsets.symmetric(vertical: 20, horizontal: 20), // 내부 여백 설정
+                contentPadding: EdgeInsets.symmetric(vertical: 16, horizontal: 16), // 내부 여백 설정
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(30.0), // 둥근 모서리 설정
                   borderSide: BorderSide.none, // 테두리 제거
